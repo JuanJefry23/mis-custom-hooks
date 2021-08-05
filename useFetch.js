ae872d543e63e2cafc4a302c0c95bf5b287674b4
Juan <og.jjoa@gmail.com>
2021-08-05 10:30:33 -0300

    Primer commit

---
+++ useFetch.js
@@ -0,0 +1,47 @@
+import { useState, useEffect, useRef } from "react";
+
+export const useFetch = (url) => {
+  const isMounted = useRef(true);
+  const [state, setState] = useState({
+    data: null,
+    loading: true,
+    error: null,
+  });
+
+  useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  useEffect(() => {
+    setState({ data: null, loading: true, error: null });
+
+    fetch(url)
+      .then((resp) => {
+        return resp.json();
+      })
+      .then((data) => {
+        setTimeout(() => {
+          if (isMounted.current) {
+            setState({
+              loading: false,
+              error: null,
+              data,
+            });
+          } else {
+            console.log("setState no se llamó");
+          }
+        }, 4000);
+      })
+      .catch(()=>{
+        setState({
+          data:null,
+          loading:false,
+          error:"No se pudo cargar la info",
+        })
+      })
+  }, [url]);
+
+  return state;
+};
